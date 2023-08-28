4fbcd16f76f94a8abfc94c4237cc43cf6ba65c06
Willmuseve <willmuseve@gmail.com>
2023-08-28 12:29:58 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# new file
+# Create a file using Puppet
 
 file { '/tmp/school':
-  ensure  => 'admin',
+  ensure  => 'present',
   content => 'I love Puppet',
-  mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data'
+  group   => 'www-data',
+  mode    => '0744'
 }
