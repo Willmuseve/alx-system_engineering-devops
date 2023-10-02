f1ee0c21c1b348e68e9a31fed020d6e0ce6dd0ba
Wilson Museve <104129182+Willmuseve@users.noreply.github.com>
2023-10-02 04:01:04 +0300

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/0-simply_match_school.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-#must match school
-puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-#matching a string that starts with h ends with n
-puts ARGV[0].scan(/^h.n$/).join
