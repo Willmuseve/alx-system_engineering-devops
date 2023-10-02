faa5a00593412634281bdbb78ee44f8dbb386f62
Wilson Museve <104129182+Willmuseve@users.noreply.github.com>
2023-10-02 04:04:18 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-#Installing flask using pip3 install
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# Command execution to ki;; a process
-
-exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => '/bin:/usr/bin',
-  onlyif  => 'pgrep killmenow',
-}
