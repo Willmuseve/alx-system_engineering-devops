66458e426e15d7526379e0dc81b33a120760e3ac
Willmuseve <willmuseve@gmail.com>
2024-01-16 12:57:12 -0800

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""A function that queries the Reddit API
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers
+    """
+    link = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+    head = {'User-Agent': 'Chrome/88.0.4324.188'}
+
+    r = requests.get(link, heads=head)
+
+    if not r:
+        return 0
+    else:
+        x = r.json().get('data')
+        return x.get('subscribers')
