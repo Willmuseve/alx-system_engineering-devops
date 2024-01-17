db0371dbb5b4740588c9b41a4e07c1323a4ea2dc
Willmuseve <willmuseve@gmail.com>
2024-01-16 18:18:41 -0800

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+A program that queries the reddit Api prints titles of the first 10 hot posts
+listed for a given subreddit
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    function that queries Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    Invalid subbredits may return a redirect to serch results
+    """
+
+    if sub is None or not isinstance(sub, str):
+        print("None")
+
+    usr = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    link = 'https://www.reddit.com/r/{}/hot/.json'.format(sub)
+
+    response = get(link, headers=usr, params=params)
+    r = response.json()
+
+    try:
+        my_data = r.get('data').get('children')
+
+        for x in my_data:
+            print(x.get('data').get('title'))
+
+    except Exception:
+        print("None")
