b0a16737802464b21efe9561c43dce8431edcc68
Willmuseve <willmuseve@gmail.com>
2024-01-16 18:38:21 -0800

    Recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
- a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit
+ a recursive function that queries the Reddit API and returns a list
+ containing the titles of all hot articles for a given subreddit
 """
 
 import requests
