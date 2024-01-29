98e4893f9cc5ebb1043594813e9baf9284621126
Willmuseve <willmuseve@gmail.com>
2024-01-29 04:32:25 +0000

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-sAscript enables user holberton to login and o
+#A script THAT enables user holberton to login
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
